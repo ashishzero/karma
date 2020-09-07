0b18f8198cc24a18c9d47d1ecbf4fb1de3f27f9b
Ashish Bhattarai <zeroa5620@gmail.com>
2020-09-07 16:02:02 +0545

    entity code cleanup

--- src/entity.h
+++ src/entity.h
@@ -40,3 +40,59 @@ struct Line : public Entity {
 	attribute("speed:0.01") Vec2 start;
 	attribute("speed:0.01") Vec2 end;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+
+struct Entity_Manager {
+	struct Entity_By_Type {
+		Array<Player>   players;
+		Array<Line>		lines;
+	};
+
+	Array<Entity_Handle>	entities;
+	Entity_By_Type			by_type;
+};
+
+template <u32>
+inline Entity *entity_down_type(Entity_Manager &manager, Entity_Handle reference) {
+	return nullptr;
+}
+#define entity_down(manager, reference, entity) (entity *)entity_down_type<Entity_##entity>(manager, reference)
+
+template <>
+inline Entity *entity_down_type<Entity_Player>(Entity_Manager &manager, Entity_Handle reference) {
+	return manager.by_type.players.data + reference.index;
+}
+
+template <>
+inline Entity *entity_down_type<Entity_Line>(Entity_Manager &manager, Entity_Handle reference) {
+	return manager.by_type.lines.data + reference.index;
+}
+
+inline Entity *entity_get(Entity_Manager &manager, Entity_Handle reference) {
+	switch (reference.kind) {
+	case Entity_Player: return entity_down_type<Entity_Player>(manager, reference);
+	case Entity_Line: return entity_down_type<Entity_Line>(manager, reference);
+	}
+	return nullptr;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+
+Entity_Manager manager_create();
+
+Entity *manager_add_entity_kind(Entity_Manager *manager, Entity_Kind kind);
+#define manager_add_entity(manager, entity) (entity *)manager_add_entity_kind(manager, Entity_##entity)
+
+Player *manager_find_player(Entity_Manager &manager, Entity_Handle handle);
+Line *manager_find_line(Entity_Manager &manager, Entity_Handle handle);
+
+Entity *manager_find_entity(Entity_Manager &manager, Entity_Handle handle);
