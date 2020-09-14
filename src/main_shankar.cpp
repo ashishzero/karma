4c4cbbd09adf4d239a52401b9a384c083019104c
Ashish Bhattarai <zeroa5620@gmail.com>
2020-09-14 13:58:40 +0545

    example server client added

--- src/main_shankar.cpp
+++ src/main_shankar.cpp
@@ -1,5 +1,85 @@
 #include "modules/core/systems.h"
 
+#define SOCKET_BUFFER_SIZE kilo_bytes(1)
+
 int karma_user_shankar() {
+	system_net_startup();
+
+	Socket_Address to = socket_address_local(9999);
+
+	auto socket = system_net_open_udp_client();
+
+	if (socket != SOCKET_INVALID) {
+		int d = (to.address & 0x000000ff) >> 0;
+		int c = (to.address & 0x0000ff00) >> 8;
+		int b = (to.address & 0x00ff0000) >> 16;
+		int a = (to.address & 0xff000000) >> 24;
+		system_trace("Client Connected to: %d.%d.%d.%d:%d", a, b, c, d, to.port);
+	}
+
+	char buffer[SOCKET_BUFFER_SIZE];
+	s32 player_x = 0;
+	s32 player_y = 0;
+
+	bool running = true;
+
+	while (running) {
+		buffer[0] = 0;
+
+		if (system_key(Key_W) || system_key(Key_UP)) {
+			buffer[0] = 'w';
+		}
+		
+		if (system_key(Key_A) || system_key(Key_LEFT)) {
+			buffer[0] = 'a';
+		}
+		
+		if (system_key(Key_S) || system_key(Key_DOWN)) {
+			buffer[0] = 's';
+		}
+		
+		if (system_key(Key_D) || system_key(Key_RIGHT)) {
+			buffer[0] = 'd';
+		}
+
+		if (system_key(Key_Q)) {
+			buffer[0] = 'q';
+			running = false;
+		}
+		
+		if (system_key(Key_ESCAPE)) {
+			running = false;
+		}
+
+		if (buffer[0]) {
+			int buffer_length = 1;
+			if (system_net_send_to(socket, buffer, buffer_length, to) < 0) {
+				system_trace("Falied to send!");
+				break;
+			}
+
+			Socket_Address from;
+			int bytes_received = system_net_receive_from(socket, buffer, SOCKET_BUFFER_SIZE, &from);
+
+			if (bytes_received < 0) {
+				system_trace("Failed to receive data");
+				break;
+			}
+
+			// grab data from packet
+			int read_index = 0;
+
+			memcpy(&player_x, &buffer[read_index], sizeof(player_x));
+			read_index += sizeof( player_x );
+
+			memcpy(&player_y, &buffer[read_index], sizeof(player_y));
+			read_index += sizeof( player_y );
+
+			memcpy(&running, &buffer[read_index], sizeof(running));
+
+			system_trace("x:%d, y:%d, is_running:%d", player_x, player_y, running);
+		}
+	}
+
 	return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,68 @@
+#include "modules/core/systems.h"
 
-int main() {
+#define SOCKET_BUFFER_SIZE kilo_bytes(1)
+
+int system_main() {	
+	system_net_startup();
+
+	Socket_Address address = socket_address_local(9999);
+	Socket socket = system_net_open_udp_server(address);
+
+	if (socket != SOCKET_INVALID) {
+		int d = (address.address & 0x000000ff) >> 0;
+		int c = (address.address & 0x0000ff00) >> 8;
+		int b = (address.address & 0x00ff0000) >> 16;
+		int a = (address.address & 0xff000000) >> 24;
+		printf("Server Launched: %d.%d.%d.%d:%d\n", a, b, c, d, address.port);
+	}
+
+	char buffer[SOCKET_BUFFER_SIZE];
+	s32 player_x = 0;
+	s32 player_y = 0;
+	bool running = true;
+
+	while (running) {
+		Socket_Address from;
+		int bytes_received = system_net_receive_from(socket, buffer, SOCKET_BUFFER_SIZE, &from);
+
+		if(bytes_received < 0) {
+			printf("Failed to receive data\n");
+			break;
+		}
+
+		int d = (from.address & 0x000000ff) >> 0;
+		int c = (from.address & 0x0000ff00) >> 8;
+		int b = (from.address & 0x00ff0000) >> 16;
+		int a = (from.address & 0xff000000) >> 24;
+
+		char client_input = buffer[0];
+		printf("%d.%d.%d.%d:%d - %c\n", a, b, c, d, from.port, client_input);
+
+		switch (client_input) {
+		case 'w': player_y += 1; break;
+		case 's': player_y -= 1; break;
+		case 'a': player_x -= 1; break;
+		case 'd': player_x += 1; break;
+		case 'q': running = false; break;
+		}
+
+		int bytes_written = 0;
+		memcpy(&buffer[bytes_written], &player_x, sizeof(player_x));
+		bytes_written += sizeof(player_x);
+
+		memcpy(&buffer[bytes_written], &player_y, sizeof(player_y));
+		bytes_written += sizeof(player_y);
+
+		memcpy(&buffer[bytes_written], &running, sizeof(running));
+		bytes_written += sizeof(running);
+
+		int bytes_send = system_net_send_to(socket, buffer, bytes_written, from);
+		if (bytes_send < 0) {
+			printf("Bytes failed to send\n");
+			break;
+		}
+
+	}
+
 	return 0;
 }
