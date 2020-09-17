fa42d5d4dc270d2bebcc05f1d4e97220890c4483
Atish Shah Rouniyar <atishshahrouniyar@gmail.com>
2020-09-17 10:32:01 +0545

    serialization working need to remove redundant code

--- src/atish.h
+++ src/atish.h
@@ -2,29 +2,58 @@
 #include "modules/core/length_string.h"
 #include "modules/core/reflection.h"
 
-struct Atishs {
-	int a;
-	int b;
-	int c;
+// Example struct
+
+struct Vector2 {
+    float x;
+    float y;
 };
 
-struct Atish {
-	enum Number {
-		ONE,
-		TWO,
-		THREE,
-		FOUR,
-		FIVE,
-	};
-	Number n = ONE;
-	int a=2;
-	int b=10;
-	String name = "Ashish";
-	attribute ("no-serialize") Atishs att = {1,2,3};
-	Atishs x[3] = { { 56,78,34 },{ 56,78,34 },{ 56,78,34 } };
-	float h=34.987f;
-	int* a_ptr;
-	Array<String> strings;
-	Array_View<int> values;
+enum Block_Type :unsigned int {
+    Block_Type_INVALID,
+    Block_Type_HARD_BLOCK,
+    Block_Type_SOFT_BLOCK,
 };
 
+struct Base {
+    unsigned int id=3;
+    Block_Type type= Block_Type_SOFT_BLOCK;
+    Block_Type type4= Block_Type_INVALID;
+    Base() {}
+    Base(Block_Type _type) : type(_type) {}
+};
+
+struct Block_Data {
+    int index;
+    unsigned int flags;
+};
+
+struct World {
+    int x;
+    int y;
+};
+
+struct Region {
+    int x;
+    int y;
+    Region(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+};
+
+struct Hard_Block : public Base {
+    Vector2 position = {3,6.9f};
+    Vector2 size = {2.1f,4.5f};
+    Block_Data data[2] = { {1,9},{3,5} };
+    String name="barud";
+    Array<int> placements = {};
+    attribute("no-serialize") World* world;
+    Region* region=new Region(3,2);
+
+    Hard_Block() : Base(Block_Type_HARD_BLOCK) {}
+};
+
+// Input to serialize function is Hard_Block, the output should be
+// The output text file should look the following way
--- src/main_atish.cpp
+++ src/main_atish.cpp
@@ -12,61 +12,71 @@ void serialize_to_file_array(FILE* fp, const Type_Info* info, char* data, int ta
 //TODO : testing 
 void serialize_to_file(FILE* fp, String name, const Type_Info* info, char* data, int tab_count = 0)
 {
-	fprintf(fp, "%*s", tab_count, "");
 	switch (info->id)
 	{
 	case Type_Id_S8:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %d,\n", string_cstr(name), (int)*(s8*)(data));
 	}
 	break;
 	case Type_Id_S16:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %d,\n", string_cstr(name), (int)*(s16*)(data));
 	}
 	break;
 	case Type_Id_S32:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %d,\n", string_cstr(name), *(s32*)(data));
 	}
 	break;
 	case Type_Id_S64:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %zd,\n", string_cstr(name), *(s64*)(data));
 	}
 	break;
 	case Type_Id_U8:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %u,\n", string_cstr(name), (u32) * (u8*)(data));
 	}
 	break;
 	case Type_Id_U16:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %u,\n", string_cstr(name), (u32) * (u16*)(data));
 	}
 	break;
 	case Type_Id_U32:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %u,\n", string_cstr(name), (u32) * (u32*)(data));
 	}
 	break;
 	case Type_Id_U64:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %zu,\n", string_cstr(name), *(u64*)(data));
 	}
 	break;
 	case Type_Id_R32:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %f,\n", string_cstr(name), *(r32*)(data));
 	}
 	break;
 	case Type_Id_R64:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %f,\n", string_cstr(name), *(r64*)(data));
 	}
 	break;
 	case Type_Id_CHAR:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "%s : %c,\n", string_cstr(name), *(char*)(data));
 	}
 	break;
@@ -77,6 +87,7 @@ void serialize_to_file(FILE* fp, String name, const Type_Info* info, char* data,
 	break;
 	case Type_Id_POINTER:// deference 
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		auto ptr_info = (Type_Info_Pointer*)info;
 		fprintf(fp, "?");
 		serialize_to_file(fp, name, ptr_info->pointer_to, (char*)(*(ptrsize*)data), tab_count - 3);
@@ -87,17 +98,22 @@ void serialize_to_file(FILE* fp, String name, const Type_Info* info, char* data,
 		break;
 	case Type_Id_ENUM:// s64 
 	{
-		//TODO :: need to handle some other things
-		fprintf(fp, "%s : %d,\n", string_cstr(name), *(s32*)(data));
+		auto enum_info = (Type_Info_Enum*)info;
+		serialize_to_file(fp, name, enum_info->item_type,data, tab_count);
 	}
 	break;
 	case Type_Id_STRUCT:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		auto struct_info = (Type_Info_Struct*)info;
 		fprintf(fp, "%s : { \n", tto_cstring(name));
-		for (int i = 0; i < struct_info->member_count; ++i)
+		if (struct_info->base)
 		{
-			auto mem = struct_info->members + i;
+			serialize_to_file(fp, ".base", struct_info->base, data, tab_count + 3);
+		}
+		for (int j = 0; j < struct_info->member_count; ++j)
+		{
+			auto mem = struct_info->members + j;
 			bool no_serialize = false;
 			for (u64 i = 0; i < mem->attribute_count; ++i)
 			{
@@ -114,30 +130,58 @@ void serialize_to_file(FILE* fp, String name, const Type_Info* info, char* data,
 		fprintf(fp, "}\n");
 	}
 	break;
-	case Type_Id_UNION:// invalid_path();
+	case Type_Id_UNION:
 	{
-		invalid_code_path();
+		fprintf(fp, "%*s", tab_count, "");
+		auto union_info = (Type_Info_Union*)info;
+		fprintf(fp, "%s : { \n", tto_cstring(name));
+		bool write = false;
+		for (int j = 0; j < union_info->member_count; ++j)
+		{
+			auto mem = union_info->members + j;
+			for (u64 i = 0; i < mem->attribute_count; ++i)
+			{
+				if (string_match(mem->attributes[i], "write"))
+				{
+					write = true;
+					break;
+				}
+			}
+			if (write)
+			{
+				serialize_to_file(fp, mem->name, mem->info, data, tab_count + 3);
+				break;
+			}
+		}
+		if (!write)
+			serialize_to_file(fp, union_info->members->name, union_info->members->info, data, tab_count + 3);
+		fprintf(fp, "%*s", tab_count, "");
+		fprintf(fp, "}\n");
 	}
 	break;
 	case Type_Id_STATIC_ARRAY:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "[N] %s : ", string_cstr(name));
 		serialize_to_file_array(fp, info, data, tab_count + 3);
 	}
 	break;
 	case Type_Id_STRING:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		String* string_data = (String*)data;
-		fprintf(fp, "%s : { %zd, \"%s\" },\n", string_cstr(name), string_data->count, tto_cstring(*string_data));
+		fprintf(fp, "%s : \"%s\" ,\n", string_cstr(name), tto_cstring(*string_data));
 	}
 	break;
 	case Type_Id_DYNAMIC_ARRAY:
 	{
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "[..] %s : ", string_cstr(name));
 		serialize_to_file_array(fp, info, data, tab_count + 3);
 	}
 	break;
 	case Type_Id_ARRAY_VIEW: {
+		fprintf(fp, "%*s", tab_count, "");
 		fprintf(fp, "[] %s : ", string_cstr(name));
 		serialize_to_file_array(fp, info, data, tab_count + 3);
 	} break;
@@ -228,45 +272,87 @@ void serialize_to_file_array(FILE* fp, const Type_Info* info, char* data, int ta
 	break;
 	case Type_Id_POINTER:// deference 
 	{
-		invalid_code_path();
+		auto ptr_info = (Type_Info_Pointer*)elem_type_info;
+		for (s64 i = 0; i < count; ++i) {
+			serialize_to_file(fp, "?", ptr_info->pointer_to, (char*)(*(ptrsize*)(data+i*sizeof(ptrsize))), tab_count - 3);
+		}
 	}
 	break;
 
 	case Type_Id_FUNCTION:// skip
 		break;
 	case Type_Id_ENUM:// s64 
+	{
+		auto enum_info = (Type_Info_Enum*)elem_type_info;
 		for (s64 i = 0; i < count; ++i) {
-			//TODO :: need to handle some other things
-			fprintf(fp, "%d, ", *(s32*)(data + i * sizeof(s32)));
+			serialize_to_file(fp, ".",enum_info->item_type, data + i * enum_info->size, tab_count - 3);
 		}
-		break;
+	}
+	break;
 	case Type_Id_STRUCT:
 	{
 		auto struct_info = (Type_Info_Struct*)elem_type_info;
 		//TODO  : need optimization
-		for (s64 i = 0; i < count; ++i) {
+		for (s64 j = 0; j < count; ++j) {
 			fprintf(fp, "%*s", tab_count, "");
 			fprintf(fp, "{\n");
+			if (struct_info->base)
+			{
+				serialize_to_file(fp, ".base", struct_info->base, data+j*struct_info->size, tab_count + 3);
+			}
 			for (int i = 0; i < struct_info->member_count; ++i)
 			{
 				auto mem = struct_info->members + i;
-				serialize_to_file(fp, mem->name, mem->info, data + i * struct_info->size + mem->offset, tab_count + 3);
+				bool no_serialize = false;
+				for (u64 k = 0; k < mem->attribute_count; ++k)
+				{
+					if (string_match(mem->attributes[k], "no-serialize"))
+					{
+						no_serialize = true;
+						break;
+					}
+				}
+				if (!no_serialize)
+					serialize_to_file(fp, mem->name, mem->info, data +j*struct_info->size+ mem->offset, tab_count + 3);
 			}
 			fprintf(fp, "%*s", tab_count, "");
 			fprintf(fp, "},\n");
-		};
+		}
 	}
 	break;
-	case Type_Id_UNION:// invalid_path();
+	case Type_Id_UNION:
 	{
-		invalid_code_path();
+		auto union_info = (Type_Info_Union*)elem_type_info;
+		for (s64 k = 0; k < count; ++k)
+		{
+			bool write = false;
+			for (int j = 0; j < union_info->member_count; ++j)
+			{
+				auto mem = union_info->members + j;
+				for (u64 i = 0; i < mem->attribute_count; ++i)
+				{
+					if (string_match(mem->attributes[i], "write"))
+					{
+						write = true;
+						break;
+					}
+				}
+				if (write)
+				{
+					serialize_to_file(fp, mem->name, mem->info, data+k*union_info->size, tab_count + 3);
+					break;
+				}
+			}
+			if (!write)
+				serialize_to_file(fp, union_info->members->name, union_info->members->info, k*union_info->size+data, tab_count + 3);
+		}
 	}
 	break;
 	case Type_Id_STRING: {
 		for (s64 i = 0; i < count; ++i) {
 			String* string_data = (String*)(data + i * sizeof(String));
 			fprintf(fp, "%*s", tab_count, "");
-			fprintf(fp, "{ %zd, \"%s\" },\n", string_data->count, tto_cstring(*string_data));
+			fprintf(fp, "\"%s\",\n",tto_cstring(*string_data));
 		};
 		break;
 	} break;
@@ -302,16 +388,9 @@ int karma_user_atish() {
 		exit(1);
 	}
 
-	Atish sample;
-	sample.a_ptr = new int(4);
-	array_add(&sample.strings, String("One"));
-	array_add(&sample.strings, String("Two"));
-	array_add(&sample.strings, String("Three"));
-
-	char* data = (char*)&sample;
-
-	const Type_Info* info = reflect_info(sample);
-	serialize_to_file(fp, info->name, info, data);
+	Hard_Block sample[4];
+	serialize_to_file(fp, "sample", reflect_info(sample), (char*)sample, 0);
+	int a = 0;
 
 #if 0
 	if (info->id == Type_Id_STRUCT) {
