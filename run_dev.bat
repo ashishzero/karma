202bcb88beb177a81776ca61d4873af8a519a6d7
Ashish Bhattarai <zeroa5620@gmail.com>
2021-02-28 15:16:14 +0545

    separate client project batch command for creating zip

--- run_dev.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd run_tree
-karma_x64_Developer.exe
-popd
--- src/main_client.cpp
+++ src/main_client.cpp
@@ -2,6 +2,10 @@
 
 #include "scene.h"
 
+#ifndef KARMA_BUILD_TYPE
+#define KARMA_BUILD_TYPE 0
+#endif
+
 int karma_client() {
 #ifdef INIT_THREAD_POOL
 	if (!async_initialize(2, mega_bytes(32), context.allocator)) {
@@ -9,7 +13,13 @@ int karma_client() {
 	}
 #endif
     
-	scene_prepare(Scene_Run_Method_CLIENT, Render_Backend_DIRECTX11, System_Window_Show_NORMAL);
+	#if (KARMA_BUILD_TYPE == 1)
+	auto run = Scene_Run_Method_CLIENT;
+	#else
+	auto run = Scene_Run_Method_DEVELOP;
+	#endif
+
+	scene_prepare(run, Render_Backend_DIRECTX11, System_Window_Show_NORMAL);
     
 	Scene* scene = scene_create();
     
